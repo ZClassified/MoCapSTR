463add450fe5a971e96de50fa9097c8dcb51893d
ZClassified <haribolord@googlemail.com>
2026-08-13 08:47:37 +0200

    Bump version to v1.1.2 and update context

--- python/main.py
+++ python/main.py
@@ -23,7 +23,7 @@ class MoCapSyncApp(ctk.CTk):
     def __init__(self):
         super().__init__()
 
-        self.title("MoCapSTR: Sync / Trigger / Record for FreeMoCap v1.1.1")
+        self.title("MoCapSTR: Sync / Trigger / Record for FreeMoCap v1.1.2")
         self.geometry("1100x800")
         
         # Managers
